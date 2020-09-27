62242df0c4567f658e0264f28b7d3ca9cc26f891
Diseyi <filomiinaa@gmail.com>
2020-09-27 16:15:40 -0700

    ""

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+}
+
+class {
+  _albumUrl = ../assets/album.json
+}
